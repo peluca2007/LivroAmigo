ea8e4c202a3b23e86425646678b10c8f23448093
Pluca2007 <pedrolucas.larini@gmail.com>
2025-10-23 20:08:35 -0300

    first commit

--- PaginaInicial/carousel.js
+++
@@ -1,8 +0,0 @@
-const images = document.querySelectorAll('.carousel-img');
-let current = 0;
-
-setInterval(() => {
-    images[current].classList.remove('active');
-    current = (current + 1) % images.length;
-    images[current].classList.add('active');
-}, 3500);
